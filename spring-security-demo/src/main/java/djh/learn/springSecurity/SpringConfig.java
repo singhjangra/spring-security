755d24a97e7f9489b6ed601e5c732fc7e9af2edb
Sunil Jangra <Sunil Jangra@Sunil>
2021-11-29 10:16:25 +0530

    Spring security demo

---
+++ spring-security-demo/src/main/java/djh/learn/springSecurity/SpringConfig.java
@@ -0,0 +1,23 @@
+package djh.learn.springSecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "djh.learn")
+@EnableWebMvc
+public class SpringConfig {
+
+	@Bean
+	 public ViewResolver configureViewResolver() {
+	     InternalResourceViewResolver viewResolve = new InternalResourceViewResolver();
+	     viewResolve.setPrefix("/WEB-INF/view/");
+	     viewResolve.setSuffix(".jsp");
+	 
+	     return viewResolve;
+	 }
+}
---
+++ spring-security-demo/src/main/java/djh/learn/springSecurity/controller/DemoController.java
@@ -0,0 +1,13 @@
+package djh.learn.springSecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+}
